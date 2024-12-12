a06387adcd61e759846a6b19c4132e4bf49c3976
Code-lab-dev <fawassaka862@gmail.com>
2024-12-12 05:48:51 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import TrackingPage from './pages/TrackingPage';
 import HistoryPage from './pages/HistoryPage';
+import LoginForm from './components/auth/LoginForm'
+import RegisterForm from './components/auth/RegisterForm'
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/track" element={<TrackingPage />} />
           <Route path="/history" element={<HistoryPage />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
         </Routes>
       </Layout>
     </Router>
