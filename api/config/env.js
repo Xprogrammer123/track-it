d4636b42739fa4a3bf735fad057ff427e94611f2
Waptech <abdulfataiyakub07@gmail.com>
2024-12-16 08:20:14 +0100

    update

--- api/config/env.js
+++ api/config/env.js
@@ -4,5 +4,5 @@ dotenv.config();
 export const config = {
   jwtSecret: '35d04417b5f1487023ba6db5220c7bfa170df2c8c1ea33c16a793b7c79c2028980fee8dd85792b42f04e6923a20cfd796bcc069efce152e519ccaa8a7f479b82',
   // port: process.env.PORT || 5000,
-  corsOrigin: process.env.CORS_ORIGIN || '*'
+  corsOrigin: '*'
 };
