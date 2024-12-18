0af3126d7e7bfa8a0d655b13acee83f9d5d634ba
Code-lab-dev <fawassaka862@gmail.com>
2024-12-18 12:57:24 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import AdminInput from "./pages/AdminInput.tsx";
+import AdminPage from "./pages/AdminPage.tsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <AdminInput />
+      <AdminPage />
     </AuthProvider>
   </StrictMode>
 );
