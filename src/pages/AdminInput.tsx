ac968a036571b7e9e1a24f93d075b3f854d980bc
Code-lab-dev <fawassaka862@gmail.com>
2024-12-18 12:45:37 +0100

    Update AdminInput.tsx

--- src/pages/AdminInput.tsx
+++ src/pages/AdminInput.tsx
@@ -168,7 +168,7 @@ const AdminUpdateForm = () => {
                 <div>
                     <button
                         type="submit"
-                        className="w-full bg-blue-500 text-white font-medium py-2 px-4 rounded-md hover:bg-blue-600"
+                        className="w-full bg-red-500 text-white font-medium py-2 px-4 rounded-md hover:bg-blue-600"
                     >
                         Update and Sync
                     </button>
