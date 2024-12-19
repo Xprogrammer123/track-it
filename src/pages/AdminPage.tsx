e40d297c929037f420348d559eff41dfb37082bb
Code-lab-dev <fawassaka862@gmail.com>
2024-12-19 17:24:29 +0100

    Update AdminPage.tsx

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -303,7 +303,16 @@ const AdminPage = () => {
           )}
         </Table>
       </TableContainer>
+      
+      {/*admin history button */}
+         <Link 
+           to ="/adhistory"
+            className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition"
+        >
+            History
+        </button>
 
+      
       {/* Pagination */}
       <TablePagination
         rowsPerPageOptions={[10, 25, 50]}
