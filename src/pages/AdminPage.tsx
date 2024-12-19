46c1ad0f1cc41769fe4086a713f5e07c8d0e022f
Code-lab-dev <fawassaka862@gmail.com>
2024-12-19 17:30:44 +0100

    Update AdminPage.tsx

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -310,7 +310,7 @@ const AdminPage = () => {
             className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition"
         >
             History
-        </button>
+        </Link>
 
       
       {/* Pagination */}
