31d4ab7055201bae98cc0b8e4d5a2d0b2894dd24
Code-lab-dev <fawassaka862@gmail.com>
2024-12-20 01:57:10 +0100

    Update Header.tsx

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -109,7 +109,7 @@ export default function Header() {
                     Login
                   </Link>
                 </div>*/}
-            .
+            
               )}
               {state.isAdmin && (
                 <Button
